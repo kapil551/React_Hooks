d657f210bc259e26c447e7cf70bc533b6ee50600
Kapil Chaudhary <chaudharikapil16@gmail.com>
2022-01-27 12:11:51 +0530

    10 - fetching data with useEffect hook part 1

--- react-hooks/src/components/functionalComponents/UseEffectHook/UseEffectFetchingData.js
+++ react-hooks/src/components/functionalComponents/UseEffectHook/UseEffectFetchingData.js
@@ -6,18 +6,20 @@ function UseEffectFetchingData() {
 
   // Data Fetching from API
   useEffect(
-      () => {
-    console.log("useEffect is called");
-    
-    axios.get("https://jsonplaceholder.typicode.com/posts")
-      .then((res) => {
-        console.log(res);
-        setPosts(res.data);
-      })
-      .catch((err) => {
-        console.log(err);
-      });
-  }, [] // I want the data to be fetched only once when the component did mount
+    () => {
+      console.log("useEffect is called");
+
+      axios
+        .get("https://jsonplaceholder.typicode.com/posts")
+        .then((res) => {
+          console.log(res);
+          setPosts(res.data);
+        })
+        .catch((err) => {
+          console.log(err);
+        });
+    },
+    [] // I want the data to be fetched only once when the component did mount
   );
 
   return (
